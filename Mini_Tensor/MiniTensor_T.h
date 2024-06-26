95ea1917ef68c1aa7b45c928f295a95d0e34285c
Jonathan <jlinpoughkeepsieday@gmail.com>
2024-06-26 13:46:49 -0500

    Add project files.

---
+++ Mini_Tensor/MiniTensor_T.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include "cuda_runtime.h"
+
+template <typename T> class MiniTensor {
+	// initialize a MiniTensor object
+public:
+	MiniTensor();
+	~MiniTensor();
+
+};
+
+template <typename T> MiniTensor<T>::MiniTensor() {
+	int gpu_count;
+	cudaGetDeviceCount(&gpu_count);
+	printf("Current GPU count: %d\n", gpu_count);
+
+	for (int current_gpu = 0; current_gpu < gpu_count; current_gpu++) {
+		cudaDeviceProp deviceProp;
+		cudaGetDeviceProperties(&deviceProp, current_gpu);
+		printf("Current device %d %s has compute capability %d.%d.\n", current_gpu, deviceProp.name, deviceProp.major, deviceProp.minor);
+		printf("Maximum number of threads per block: %d\n", deviceProp.maxThreadsPerBlock);
+		printf("Wrap size: %d\n", deviceProp.warpSize);
+		printf("Max Threads Dimension: (%d, %d, %d)\n", deviceProp.maxThreadsDim[0], deviceProp.maxThreadsDim[1], deviceProp.maxThreadsDim[2]);
+		printf("Max Grid Size (%d, %d, %d)\n", deviceProp.maxGridSize[0], deviceProp.maxGridSize[1], deviceProp.maxGridSize[2]);
+		printf("Shared Memory Per Block: %d bytes\n", deviceProp.sharedMemPerBlock);
+	}
+}
+
+template <typename T> MiniTensor<T>::~MiniTensor() {
+}
---
+++ Mini_Tensor/main.cpp
@@ -0,0 +1,7 @@
+#include "MiniTensor_T.h"
+
+int main() {
+	MiniTensor<int> *mt = new MiniTensor<int>();
+	delete mt;
+	return 0;
+}
