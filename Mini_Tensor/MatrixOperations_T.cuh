cff505538115b501a3394b658b3d44ecdd6ddb99
Jonathan <jlinpoughkeepsieday@gmail.com>
2024-07-09 14:35:52 -0500

    Removed template default argument (removed int) as it is redundant.

--- Mini_Tensor/MatrixOperations_T.cuh
+++ Mini_Tensor/MatrixOperations_T.cuh
@@ -1,24 +1,30 @@
 // MatrixOperations_T.cuh
-// The current design for MatrixOperations_T is that it performs on the device memory.
+
 #ifndef MINI_TENSOR_MATRIXOPERATIONS_CUH
 #define MINI_TENSOR_MATRIXOPERATIONS_CUH
 
 #include "utils.h"
 
-template<typename T = int> __global__ void kernel_matrixAdd(T* input_1, T* input_2, T* output, long long total_size) {
+template<typename T> __global__ void kernel_matrixAdd(T* input_1, T* input_2, T* output, long long total_size) {
 	int thread_id = blockId.x * blockDim.x + threadIdx.x;
 	if (thread_id < total_size) {
 		output[thread_id] = input_1[thread_id] + input_2[thread_id];
 	}
 }
 
-template <typename T = int> class MatrixOperations {
+/// <summary>
+/// The current design for MatrixOperations_T is that it reiceves input and output as pointers to host memory 
+/// and perform operations in the device memory.
+/// </summary>
+/// <typeparam name="T"> Any data type. </typeparam>
+template <typename T> class MatrixOperations {
 public:
 	MatrixOperations();
 	MatrixOperations(long long rows, long long cols);
 	~MatrixOperations();
 
 	void add(T* input_1, T* input_2, T* output);
+
 private:
 	long long rows;
 	long long cols;
@@ -56,6 +62,17 @@ MatrixOperations<T>::~MatrixOperations() {
 	cudaCheckError(cudaFree(d_input_1));
 	cudaCheckError(cudaFree(d_input_2));
 	cudaCheckError(cudaFree(d_output));
+}
+
+/// <summary>
+/// Perform matrix addition given two inputs.
+/// </summary>
+/// <param name="input_1"> input 1 from host memory. </param>
+/// <param name="input_2"> input 2 from host memory. </param>
+/// <param name="output">  output from host memory. </param>
+template <typename T>
+void MatrixOperations<T>::add(T* input_1, T* input_2, T* output) {
+
 }
 
 #endif 
